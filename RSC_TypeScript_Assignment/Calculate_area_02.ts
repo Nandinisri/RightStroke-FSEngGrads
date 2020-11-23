80305b60d3ea8de43db915dc9a9acd074c6b1388
Nandini Srivastava <44871328+Nandinisri@users.noreply.github.com>
2020-11-23 18:20:22 +0530

    Update Calculate_area_02.ts

--- RSC_TypeScript_Assignment/Calculate_area_02.ts
+++ RSC_TypeScript_Assignment/Calculate_area_02.ts
@@ -1,5 +1,5 @@
 export class Triangle{
-     tri_area=(h:number,b:number)=>{
+     area=(h:number,b:number)=>{
         return 0.5*h*b;
      }
 
@@ -24,18 +24,13 @@ export class Triangle{
         this.hei=hei;
     }
     rect_area=()=>{
-        console.log("The surface area of a rectangle is "+2*((this.len*this.bre)+(this.bre*this.hei)+(this.hei*this.len)))
+        console.log("surface area of a rectangle is "+2*((this.len*this.bre)+(this.bre*this.hei)+(this.hei*this.len)))
     }
 }
 let rectangle:Rectangle = new Rectangle(3,4,3);
 rectangle.rect_area();
 let square:Square = new Square(3);
-console.log("The surface area of the square is "+square.squa_area());
+console.log("surface area of the square is "+square.squa_area());
 let triangle:Triangle =new Triangle();
-console.log("The Surface area of the triangle "+triangle.tri_area(2,2));
+console.log("Surface area of the triangle "+triangle.area(2,2));
 
-/*
-The surface area of a rectangle is 66
-The surface area of the square is 9
-The Surface area of the triangle 2
-*/
